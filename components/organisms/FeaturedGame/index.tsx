4fad5228ac5e57097d0c90cc81fb63c3135fd5f1
idmzulkarnaen <kreatifku1995@gmail.com>
2021-11-03 03:46:55 +0700

    configurati API Services

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,15 +1,18 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { getFeaturedGame } from "../../../services/player";
 import GameItem from "../../molecules/GameItem";
 
 
 export default function FeaturedGame() {
   const [gameList, setGameList] = useState([]);
 
-  useEffect(async () => {
-    const response = await axios.get('https://vcgame.herokuapp.com/api/v1/players/landingpage')
-    console.log('data ', response.data);
-    setGameList(response.data.data);
+  const getFeatureGameList = useCallback(async () => {
+    const data = await getFeaturedGame();
+    setGameList(data);
+  }, [getFeaturedGame]);
+
+  useEffect(() => {
+    getFeatureGameList();
   }, [])
   return (
     <section className="featured-game pt-50 pb-50">
